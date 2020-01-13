323912fa8b976ba613bb2944057080f5623b71c7
sing8968 <sing8968@sheridancollege.ca>
2020-01-13 17:23:09 -0500

    I COMMITTED THE COMMIT

---
+++ src/week1/DiffrenceInTime.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week1;
+
+/**
+ *
+ * @author hp
+ */
+public class DiffrenceInTime {
+    /**
+     * 
+     * @param s
+     * @return 
+     */
+    public static String getTotalSeconds(String s)
+    {
+        String s1 = s;
+        String[] words;
+        words = s1.split(":");
+    
+    }
+}
